4691a1253a32d6f9c1fe1d7dc64aab93ba5cf3cd
Valerie <157377780+jourdefeu@users.noreply.github.com>
2025-11-07 00:24:13 +0300

    Update test_detector.py

--- try/test_detector.py
+++ try/test_detector.py
@@ -5,14 +5,25 @@ import os
 detector = FaceDetector(device="cpu")
 
 input_dir = "try/photos/raw"
-output_dir = "try/photos/detected"
-os.makedirs(output_dir, exist_ok=True)
+detected_dir = "try/photos/detected"
+aligned_dir = "try/photos/aligned"
+
+os.makedirs(detected_dir, exist_ok=True)
+os.makedirs(aligned_dir, exist_ok=True)
 
 # проход по всем изображениям
 for filename in os.listdir(input_dir):
     if filename.lower().endswith((".jpg", ".jpeg", ".png", ".webp")):
         in_path = os.path.join(input_dir, filename)
-        out_path = os.path.join(output_dir, filename)
+        out_path = os.path.join(detected_dir, filename)
+
+        # детекция и рамки
         success = detector.detect_and_draw(in_path, out_path)
         if success:
-            print(f"💾 Сохранено: {out_path}")
+            print(f"💾 Фото с обнаруженными лицами сохранено: {out_path}")
+
+            # выравнивание и сохранение отдельных лиц
+            # для каждого исходного фото будет своя подпапка с выровненными лицами
+            aligned_dir_for_file = os.path.join(aligned_dir, os.path.splitext(filename)[0])
+            os.makedirs(aligned_dir_for_file, exist_ok=True)
+            detector.align_from_detected(in_path, aligned_dir_for_file)
